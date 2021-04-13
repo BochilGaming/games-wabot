b77534fc8b7d822bb92f168c071185a84e5f4ae5
Dhamz GAMING <78997366+idhamthoriqbot@users.noreply.github.com>
2021-04-13 09:32:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  'https://api.xteam.xyz': 'dhamzbot'
 }
 
 // Sticker WM
