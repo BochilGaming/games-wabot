8f4ce1969558604adcb4937e2f1e9b5dcb77c41f
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-04-24 13:29:30 +0700

    Downgrade to baileys 3.4.1

--- plugins/join.js
+++ plugins/join.js
@@ -3,7 +3,10 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 let handler = async (m, { conn, text }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link invalid'
-    let res = await conn.acceptInvite(code)
+    let res = await conn.query({
+        json: ["action", "invite", code]
+    })
+    if (res.status !== 200) throw res
     m.reply(`Berhasil join grup ${res.gid}`)
 }
 handler.help = ['join <chat.whatsapp.com>']
@@ -11,6 +14,6 @@ handler.tags = ['premium']
 
 handler.command = /^join$/i
 
-handler.premium = true
+handler.prems = true
 
 module.exports = handler
