b37a26f5739a0909f8dfc232cddd0319f6854c87
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-05-06 09:00:49 +0700

    Update antilink

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,15 +1,26 @@
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = function (m, { user }) {
+handler.before = function (m, { user, bot, groupMetadata }) {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    m.reply((user.isAdmin || user.isSuperAdmin) ? '*Kamu admin, kamu gk bakal dikick*'  : '*Byee, kamu akan di kick!!*')
-    if ((user.isAdmin || user.isSuperAdmin)) return true
-    conn.groupRemove(m.chat, [m.sender])
+    m.reply('*Byee, kamu akan di kick!!*')
+    if (user.isAdmin || user.isSuperAdmin) return m.reply('*Eh maap kamu admin, kamu gk bakal dikick*')
+    let participants = m.isGroup ? groupMetadata.participants : []
+    let bot = m.isGroup ? participants.find(u => u.jid == this.user.jid) : {}
+    if (bot.isAdmin || bot.isSuperAdmin) {
+        let linkGC = this.groupInviteCode(m.chat)
+        let isLinkThisGc = new RegExp(linkGC, 'g')
+        let isgclink = isLinkThisGc.exec(m.text)
+        if (isgclink) { 
+             m.reply('*Lol ngirim link group sendiri :v*')
+        } else {
+             this.groupRemove(m.chat, [m.sender])
+        }
+    } else m.reply('*Bot bukan admin, mana bisa kick orang _-*')
   }
   return true
 }
