bdfa0721b71e0a4e0d9fa583f81b5551bd46aa57
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-05-15 10:03:19 +0700

    Fix bug wm in sticker and add wm costum
    Fix bug in issue #16

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,20 +1,21 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
 
-let handler = async (m, { conn, args, DevMode }) => {
+let handler = async (m, { conn, text, DevMode }) => {
     let stiker = false
     try {
+        let [packname, author] = text.split`|`
+        if (!packname) packname = global.packname
+        if (!author) author = global.author
         let q = m.quoted ? m.quoted : m 
         let mime = (q.msg || q).mimetype || ''
         if (/image|video/.test(mime)) {
             let img = await q.download()
             if (!img) throw 'Foto/Video tidak ditemukan'
-            stiker = await sticker(img, false, mime,  global.packname, global.author)
-        } else if (args[0]) stiker = await sticker(false, args[0], false, global.packname, global.author)
-        else m.reply('Tag foto/videonya!!')
+            stiker = await sticker(img, false, mime, packname, author)
+        } else m.reply('Tag foto/videonya!!')
     } catch (e) {
         console.log(e)
-        m.reply('Error!!')
         if (DevMode) {
             let file = require.resolve(__filename)
             for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
@@ -28,9 +29,9 @@ let handler = async (m, { conn, args, DevMode }) => {
         else return m.reply('Conversasion failed!! ')
     }
 }
-handler.help = ['stiker (caption|reply media)', 'sgif (caption|reply media)']
+handler.help = ['wm'].map(v => v + ' packname|author')
 handler.tags = ['sticker']
-handler.command = /^(s(tic?ker)?(gif)?)$/i
+handler.command = /^(wm)$/i
 
 handler.fail = null
 
