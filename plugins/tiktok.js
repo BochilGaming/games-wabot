2beb0d9b961a66d0e037919e499af0c89a6eb22f
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-05-17 16:24:47 +0700

    Add more fitur
    This fitur from [`this`](https://github.com/Nurutomo/games-wabot)

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -4,13 +4,15 @@ let handler = async (m, { conn, args }) => {
   let res = await fetch(global.API('xteam', '/dl/tiktok', {
     url: args[0]
   }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
   let json = await res.json()
-  if (!json.result) throw json
-  conn.sendFile(m.chat, json.result.url, 'tiktok.mp4', `
-_*Username:*_ @${json.result.username}
-`.trim(), m, false, {
-    thumbnail
-  })
+  if (!json.status) throw json
+  try {
+    await conn.sendFile(m.chat, json.server_1, 'tiktok.mp4', '', m)
+  } catch (e) {
+    m.reply('Server 1 Failed, Retrying with Server 2')
+    await conn.sendFile(m.chat, json.server_2, 'tiktok.mp4', '', m)
+  }
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
