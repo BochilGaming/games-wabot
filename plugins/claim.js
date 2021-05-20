a8169cfa7c0898c76892fcc3cdb76dc654753603
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-05-20 15:19:51 +0700

    Remove extra line

--- plugins/claim.js
+++ plugins/claim.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let _timers = (43200000 - __timers)
     let timers = clockString(_timers) 
     if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 43200000) {
-        conn.reply(m.chat, `Anda sudah mengklaim dan mendapatkan 1000 money dan 1 potion`, m)
+        conn.reply(m.chat, `Kamu sudah mengklaim dan mendapatkan 1000 money dan 1 potion`, m)
         global.DATABASE._data.users[m.sender].money += 1000
         global.DATABASE._data.users[m.sender].potion += 1
         global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
@@ -12,23 +12,11 @@ let handler = async (m, { conn }) => {
 handler.help = ['claim']
 handler.tags = ['rpg']
 handler.command = /^(claim)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
 
 handler.fail = null
-handler.money = 0
 
 module.exports = handler
 
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
 function clockString(ms) {
   let h = Math.floor(ms / 3600000)
   let m = Math.floor(ms / 60000) % 60
