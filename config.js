e11af52e9f4c88d1354349ab54a6aa0c43188348
Ari Sawali <arisawali2020@gmail.com>
2021-05-24 10:53:56 +0700

    Fix error typo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM']
-global.owner = ['6281390658325', '6282256115584', '6285713964963']// Put your number here
+global.owner = ['6289651157752',]// Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -15,7 +15,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = 'Ari Sawali'
 
 global.multiplier = 69 // The higher, The harder levelup
 
