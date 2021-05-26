2356eb8754a69d637a95fec56c5d5016bba3a238
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-05-26 17:02:30 +0700

    New fitur tebakgambar

---
+++ plugins/tebakgambar.js
@@ -0,0 +1,33 @@
+let fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 4999
+let handler  = async (m, { conn, usedPrefix }) => {
+    conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
+    let id = m.chat
+    if (id in conn.tebakgambar) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakgambar[id][0])
+        throw false
+    }
+    let res = await fetch('https://zahirr-web.herokuapp.com/api/kuis/tebakgambar?apikey=zahirgans')
+    let json = await res.json()
+    if (json.status) throw json
+    let caption = `
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}hint untuk hint
+Bonus: ${poin} XP
+    `.trim()
+    conn.tebakgambar[id] = [
+      await conn.sendFile(m.chat, json.result.images, 'tebakgambar.jpg', caption, m),
+      json, poin,
+      setTimeout(() => {
+        if (conn.tebakgambar[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.tebakgambar[id][0])
+        delete conn.tebakgambar[id]
+      }, timeout)
+    ]
+  }
+  handler.help = ['tebakgambar']
+  handler.tags = ['game']
+  handler.command = /^tebakgambar/i
+  
+  module.exports = handler
