1a66914263a1a09a73658ebe56baa20fd953eb2e
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-06-15 14:55:50 +0700

    Add conn in request

--- plugins/request.js
+++ plugins/request.js
@@ -1,35 +1,26 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text }) => {
+    conn.req = conn.req ? conn.req : {}
     if (!text) return conn.reply(m.chat, 'Mau request apa an?', m) 
-    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-    let _text = ('dari *' + (name.vnmae || name.notify || name.name) + '*\nNo: *' + m.sender.split`@`[0] + '*\nRequest:')
+    let _name = m.fromMe ? conn.user : conn.contacts[m.sender]
+    let name = _name.vnmae || _name.notify || _name.name || ('+' + _name.jid.split`@`[0])
+    let _text = ('dari *' + name + '*\nNo: *' + m.sender.split`@`[0] + '*\nRequest:')
     let txt = (text)
     conn.reply(m.chat, 'Pesan Anda sudah terkirim', m)
+    conn.req[m.sender] = {
+        name,
+        text,
+        date: new Date
+    }
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
        m.reply(_text, jid)
        m.reply(txt, jid)
     }
 }
-handler.help = ['request <teks>?']
+handler.help = ['request', 'report'].map(v => v + '[teks]')
 handler.tags = ['main']
-handler.command = /^(re(quest|port)|req)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
+handler.command = /^(re(quest|port)|req|bug)$/i
 
 handler.fail = null
 
 module.exports = handler
-
-function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
-  let m = Math.floor(ms / 60000) % 60
-  let s = Math.floor(ms / 1000) % 60
-  console.log({ms,h,m,s})
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
