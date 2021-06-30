268b8484bd12cbd18903a5ba270d3982f36f93a2
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-06-30 13:01:15 +0700

    AntiToxic nya ngeselin Kata Ilham :v

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -3,6 +3,7 @@ let handler = m => m
 let linkRegex = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
 handler.before = function (m, { user }) {
   if (m.isBaileys && m.fromMe) return true
+  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupToxic = linkRegex.exec(m.text)
 
