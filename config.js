55cbccc4c6734e5da7724f2062c392176607c7da
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-07-10 08:24:11 +0700

    gini kan?

--- config.js
+++ config.js
@@ -14,7 +14,8 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.dhamzxploit.my.id': 'dzxganz'
 }
 
 // Sticker WM
