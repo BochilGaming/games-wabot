5189531f9e893320a58d4ae39f9259215c266467
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-07-19 12:28:32 +0700

    Update resetlimit.js

--- plugins/resetlimit.js
+++ plugins/resetlimit.js
@@ -10,10 +10,10 @@ let handler = async (m, { conn, args }) => {
 	list.slice(0, list.length).map(([user, data], i) => (Number(data.limit = lim)))
 		conn.reply(m.chat, `*berhasil direset ${lim} / user*`, m)
 }
-handler.help = ['']
-handler.tags = ['']
+handler.help = ['limit'].map(v => 'reset' + v)
+handler.tags = ['owner', 'host']
 handler.command = /^(resetlimit)$/i
+
 handler.owner = true
-handler.exp = 0
-handler.fail = null
+
 module.exports = handler
