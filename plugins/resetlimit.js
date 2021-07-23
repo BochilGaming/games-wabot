1f569a6f28cbbe936202b54c370b9782264a0197
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-07-23 13:45:11 +0700

    Update resetlimit.js

--- plugins/resetlimit.js
+++ plugins/resetlimit.js
@@ -2,12 +2,9 @@ let { Presence } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
 	await conn.updatePresence(m.chat, Presence.composing) 
 	let list = Object.entries(global.DATABASE.data.users)
-	if(!args || !args[0]) {
-		var lim = 100
-	} else {
-		var lim = parseInt(args[0])
-	}
-	list.slice(0, list.length).map(([user, data], i) => (Number(data.limit = lim)))
+	let lim = !args || !args[0] ? 100 : isNumber(args[0]) ? parseInt(args[0]) : 100
+	lim = Math.max(1, lim)
+	list.map(([user, data], i) => (Number(data.limit = lim)))
 		conn.reply(m.chat, `*berhasil direset ${lim} / user*`, m)
 }
 handler.help = ['limit'].map(v => 'reset' + v)
@@ -17,3 +14,8 @@ handler.command = /^(resetlimit)$/i
 handler.rowner = true
 
 module.exports = handler
+
+function isNumber(x = 0) {
+  x = parseInt(x)
+  return !isNaN(x) && typeof x == 'number'
+}
