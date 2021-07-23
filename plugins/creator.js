05a2b2d767e12a70be47137a18fd6a7f1794fbcc
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-07-23 13:34:18 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,6 @@
 let handler = function (m) {
-  this.fakeReply(m.chat, 'This is my owner', '0@s.whatsapp.net', 'Owner')
+  // this.fakeReply(m.chat, 'This is my owner', '6281390658325@s.whatsapp.net', 'Owner')
   this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  //plsss do not change this, jika mau add boleh tapi jangan rubah
   this.sendContact(m.chat, '6281390658325', 'Owner Metro Bot :)', m)
 }
 handler.help = ['owner', 'creator']
