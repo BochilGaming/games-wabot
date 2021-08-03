8b26b1842fb830886015cc0076c5d61a780f4ea0
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-08-03 13:51:15 +0700

    Update jadikatalog.js

--- plugins/jadikatalog.js
+++ plugins/jadikatalog.js
@@ -2,25 +2,25 @@
 JUST EXPERIMENT
 Tqto Nayla
 Plug by damzz
+recode by BochilGaming
 */
 let split = '|'
-let handler  = async (m, { conn, text }) => {
-try{
+let handler  = async (m, { conn, text, usedPrefix }) => {
   let lmao = 'emror?'
   let [text, ...text2] = txt.replace(lmao, '').trimStart().split(split)
   text2 = text2.join(split)
-  if (!text) return conn.reply(m.chat, 'Masukan Judul dan Deskripsi nya', m)
-	if (!text2) return conn.reply(m.chat, 'Contoh : .jadikatalog SLAYER?|INI BUKAN SELAYER BANG:>', m)
-let q = m.quoted ? m.quoted : m 
+  if (!text) throw 'Masukan Judul dan Deskripsi nya'
+  if (!text2) throw `Contoh : ${usedPrefix}jadikatalog SLAYER?|INI BUKAN SELAYER BANG:>`
+  let q = m.quoted ? m.quoted : m 
   let mime = (q.msg || q).mimetype || ''
   if (/image|video/.test(mime)) {
     let img = await q.download()
     if (!img) throw 'Foto/Sticker tidak ditemukan'
-let pi = conn.prepareMessageFromContent(m.chat, {
+    let pi = conn.prepareMessageFromContent(m.chat, {
 	"productMessage": { "product": { "productImage":{ "mimetype": "image/jpeg", "jpegThumbnail": img }, "title": `${text}`, "description": `${text2}`, "currencyCode": "IDR", "priceAmount": "50000", "retailerId": 'DZX Bot', "productImageCount": 1 }, "businessOwnerJid": `13479805233@s.whatsapp.net`
-}}, {})} else m.reply('FOTO NYA MANA OM?')
- conn.relayWAMessage(pi, m)
-}
+}}, {})
+    conn.relayWAMessage(pi, m)
+  } else m.reply('FOTO NYA MANA OM?')
 }
 
 handler.help = ['jadikatalog']
