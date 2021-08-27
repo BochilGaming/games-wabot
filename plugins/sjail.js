5b2e607ea397d9dadd1eb068404329381d4799bd
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-08-27 12:39:08 +0700

    Update sjail.js

--- plugins/sjail.js
+++ plugins/sjail.js
@@ -11,8 +11,8 @@ let handler = async (m, { conn, text }) => {
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
   let url = await uploadImage(img)
-  let wanted = `https://api.dhamzxploit.my.id/api/canvas/wanted?url=${url}`
-  let stiker = await sticker(null, wanted, 'Jail', '©games-wabot')
+  let wanted = global.API('dzx', '/api/canvas/wanted', { url }) //`https://api.dhamzxploit.my.id/api/canvas/wanted?url=${url}`
+  let stiker = await sticker(null, wanted, 'jail', '©games-wabot')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
@@ -20,18 +20,8 @@ let handler = async (m, { conn, text }) => {
    m.reply('Conversion Failed')
   }
 }
-handler.help = ['Jail']
+handler.help = ['jail']
 handler.tags = ['sticker']
-handler.command = /^Jail$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.command = /^jail$/i
 
 module.exports = handler
