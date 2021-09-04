a2b6e8bc834d61bf399c7ca9dc3eef3a1b246623
Idham Thoriq <78997366+idhamthoriqbot@users.noreply.github.com>
2021-09-04 19:35:45 +0700

    Hmmm Ilegal?
    bukan bug

---
+++ plugins/jadiyt.js
@@ -0,0 +1,27 @@
+let fs = require ('fs')
+let path = require('path')
+let split = '|'
+let handler  = async (m, { conn, text: txt, usedPrefix, command }) => {
+let dude = 'emror?'
+let [text, ...text2] = txt.replace(dude, '').trimStart().split(split)
+  text2 = text2.join(split)
+if (!text) throw `Reply foto dengan caption:${usedPrefix + command} Test|Proto`
+let ppnya = await fs.readFileSync(path.join(__dirname , '../src/damzz.jpg')).toString('base64')
+
+conn.sendMessage(m.chat, 'Gk bisa ya?:v', 'extendedTextMessage', { thumbnail: ppnya, jpegThumbnail: ppnya, contextInfo : { mentionedJid: m.sender,
+    externalAdReply: {
+                    title: `${text}`,
+                    body: `${text2}`,
+                    mediaType: 2,
+                    mediaUrl: 'https://youtu.be/5qap5aO4i9A',
+                    thumbnailUrl: await conn.getProfilePicture(m.sender)
+                }}})
+
+
+
+}
+handler.help = ['jadiyt teks|teks']
+handler.tag = ['tools']
+handler.command = ['jadiyt']
+
+module.exports = handler
