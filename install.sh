40cbc6f727d04a0fb55798f671a5dd32a96dd225
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-09-05 09:42:46 +0700

    Fix?

--- install.sh
+++ install.sh
@@ -2,66 +2,65 @@ pkg update && pkg upgrade
 apt update && apt upgrade
 bin=$PREFIX/bin
 dir=$(pwd)
-alias ee='echo -e'
 if [ -e $bin/node ]; then 
-  ee "\u001b[32mSkip install nodejs but already installed!"
+  echo -e "\u001b[32mSkip install nodejs but already installed!"
 else 
-  ee "\u001b[31mInstaling \u001b[31mnodejs!"
+  echo -e "\u001b[31mInstaling \u001b[31mnodejs!"
   pkg install nodejs -y
-  ee "\u001b[32mDone install nodejs!"
+  echo -e "\u001b[32mDone install nodejs!"
 fi
 
 if [ -e $bin/ffmpeg ]; then 
-  ee "\u001b[32mSkip install ffmpeg but already installed!"
+  echo -e "\u001b[32mSkip install ffmpeg but already installed!"
 else
-  ee "\u001b[31mInstaling \u001b[31mffmpeg!"
+  echo -e "\u001b[31mInstaling \u001b[31mffmpeg!"
   pkg install ffmpeg -y
-  ee "\u001b[32mDone install ffmpeg!"
+  echo -e "\u001b[32mDone install ffmpeg!"
 fi
 
 if [ -e $bin/magick ]; then 
-  ee "\u001b[32mSkip install imagemagick but already installed!"
+  echo -e "\u001b[32mSkip install imagemagick but already installed!"
 else
-  ee "\u001b[31mInstaling \u001b[31mimagemagick!"
+  echo -e "\u001b[31mInstaling \u001b[31mimagemagick!"
   pkg install imagemagick -y
-  ee "\u001b[32mDone install imagemagick!"
+  echo -e "\u001b[32mDone install imagemagick!"
 fi
 
-ee "\u001b[33mChecking instalation!"
+echo -e "\u001b[33mChecking instalation!"
 check
-ee "\u001b[32mDone checking instalation!" 
+echo -e "\u001b[32mDone checking instalation!" 
 
-ee "\u001b[33mInstall and update module!"
+echo -e "\u001b[33mInstall and update module!"
 if [ -e $dir/package.json ]; then
   npm install
   npm update
-  ee "\u001b[32Done Install and update module!" 
+  echo -e "\u001b[32Done Install and update module!" 
 else 
-  ee "\u001b[31mPackage.json not found!"
+  echo -e "\u001b[31mPackage.json not found!"
 fi 
 
-ee "\u001b[32mStarting bot..."
-ee "\u001b[32mIf found bug pls report in https://github.com/Nurutomo/wabot-aq/issues/new?assignees=&labels=&template=bug_report.md&title="
+echo -e "\u001b[32mStarting bot..."
+echo -e "\u001b[32mIf found bug pls report in https://github.com/Nurutomo/wabot-aq/issues/new?assignees=&labels=&template=bug_report.md&title="
 npm run start
 
 
 function check() {
   if [ -e $bin/node ]; then
-    ee "\u001b[32mNodejs already installed!"
+    echo -e "\u001b[32mNodejs already installed!"
   else
-    ee "\u001b[31mNodejs not installed!!, install manualy using `pkg install nodejs -y` or `apt install nodejs -y`"
+    echo -e "\u001b[31mNodejs not installed!!, install manualy using `pkg install nodejs -y` or `apt install nodejs -y`"
     exit 1
   fi 
 
   if [ -e $bin/ffmpeg ]; then
-    ee "\u001b[32mFfmpeg already installed!" 
+    echo -e "\u001b[32mFfmpeg already installed!" 
   else
-    ee "\u001b[31mFfmpeg not installed!!, install manualy using `pkg install ffmpeg -y` or `apt install ffmpeg -y`"
+    echo -e "\u001b[31mFfmpeg not installed!!, install manualy using `pkg install ffmpeg -y` or `apt install ffmpeg -y`"
   fi 
 
   if [ -e $bin/magick ]; then
-    ee "\u001b[32mImagemagick already installed!" 
+    echo -e "\u001b[32mImagemagick already installed!" 
   else
-    ee "\u001b[31mImagemagick not installed!!, install manualy using `pkg install imagemagick -y` or `apt install imagemagick -y`"
+    echo -e "\u001b[31mImagemagick not installed!!, install manualy using `pkg install imagemagick -y` or `apt install imagemagick -y`"
   fi 
 }
--- install2.sh
+++ install2.sh
@@ -1,44 +1,44 @@
 bin=$PREFIX/bin
 dir=$(pwd)
-alias ee='echo -e'
+normal='\e[0m'
 if [ -e $bin/git ]; then 
-  ee "\u001b[32mSkip install git but already installed!"
+  echo -e "\u001b[32mSkip install git but already installed!"
 else 
-  ee "\u001b[31mInstaling \u001b[31mgit!"
+  echo -e "\u001b[31mInstaling \u001b[31mgit!"
   pkg install nodejs -y
-  ee "\u001b[32mDone install git!"
+  echo -e "\u001b[32mDone install git!"
 fi
 
-ee "\u001b[33mChecking instalation!"
+echo -e "\u001b[33mChecking instalation!"
 check
-ee "\u001b[32mDone checking instalation!" 
+echo -e "\u001b[32mDone checking instalation!" 
 
-ee "\u001b[36mCloning reposito!"
+echo -e "\u001b[36mCloning reposito!"
 git clone https://github.com/BochilGaming/games-wabot.git games-wabot
 if [ -d $dir/games-wabot ]; then
-  ee "\u001b[36mDone clone reposito!"
+  echo -e "\u001b[36mDone clone reposito!"
 else
-  ee "\u001b[31mFail to clone reposito!"
+  echo -e "\u001b[31mFail to clone reposito!"
   exit 1
 fi
 
-ee "\u001b[36mChange working directory!"
+echo -e "\u001b[36mChange working directory!"
 cd games-wabot
 main_dir=$(pwd)
 if [ $main_dir !== $dir ]; then
-  ee "\u001b[36mDone change working directory!"
-  ee "\u001b[36mInstalling bot!"
+  echo -e "\u001b[36mDone change working directory!"
+  echo -e "\u001b[36mInstalling bot!"
   bash install.sh
 else 
-  ee "\u001b[31mFail to change working directory!!"
+  echo -e "\u001b[31mFail to change working directory!!"
   exit 1
 fi
 
 function check() {
   if [ -e $bin/git ]; then
-    ee "\u001b[32mGit already installed!"
+    echo -e "\u001b[32mGit already installed!"
   else
-    ee "\u001b[31mGit not installed!!, install manualy using `pkg install git -y` or `apt install git -y` and after that, type `bash install2.sh`"
+    echo -e "\u001b[31mGit not installed!!, install manualy using `pkg install git -y` or `apt install git -y` and after that, type `bash install2.sh`"
     exit 1
   fi
 }
