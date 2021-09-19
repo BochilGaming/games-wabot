a46495c44ca1fb7447f529b2c82aa944c56dade4
zatu22 <74061098+zatu22@users.noreply.github.com>
2021-09-19 22:02:29 +0200

    Update random_meme.js
    added conn.sendButton

--- plugins/random_meme.js
+++ plugins/random_meme.js
@@ -1,5 +1,3 @@
-//created by zatu22, WilfredAlmeida
-
 let fetch = require('node-fetch');
 let fs = require('fs');
 let path = require('path');
@@ -7,7 +5,7 @@ let path = require('path');
 let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, text }) => {
- 
+
     var IMAGE__URL = ""
     await fetch("https://meme-api.herokuapp.com/gimme").then(res => res.text()).then(text => {
         jso = JSON.parse(text)
@@ -15,22 +13,8 @@ let handler = async(m, { conn, text }) => {
         IMAGE__title = jso.title
         console.log(`Image URL ${IMAGE__URL}`);
     }).catch((error) => console.log("ERROR: ", error));
-
-    let buttons = button('©Reddit')
-    let msgtitel = IMAGE__title
-
-    await conn.sendFile(
-            m.chat,
-            IMAGE__URL,
-            '*',
-            msgtitel,
-            m,
-       ),           
-        conn.sendMessage(
-            m.chat,
-            buttons,
-            MessageType.buttonsMessage,
-        )
+    
+    conn.sendButton(m.chat, titel = IMAGE__title, footer = ('ChatBot'), buffer = IMAGE__URL, button = [["/meme"]])
 }
 
 handler.help = ['meme']
@@ -40,20 +24,3 @@ handler.command = /^meme$/i
 handler.fail = null
 
 module.exports = handler
-
-function button(teks) {
-
-    const buttons = [
-        { buttonId: 'id1', buttonText: { displayText: '/menu' }, type: 1 },
-        { buttonId: 'id2', buttonText: { displayText: '/meme' }, type: 1 }
-    ]
-
-    const buttonMessage = {
-        contentText: teks,
-        footerText: '©Chatbot',
-        buttons: buttons,
-        headerType: 1
-    }
-
-    return buttonMessage
-}
