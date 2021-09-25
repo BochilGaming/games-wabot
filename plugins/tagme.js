f471077129804df99083453c2017574407ad4f7c
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-09-25 17:18:47 +0700

    Update tagme.js

--- plugins/tagme.js
+++ plugins/tagme.js
@@ -1,15 +1,12 @@
 let handler = async (m, { conn, text }) => {
-
-let orang = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-let tag = `@${m.sender.replace(/@.+/, '')}`
-let mentionedJid = [m.sender]
-
+  let tag = `@${m.sender.replace(/@.+/, '')}`
+  let mentionedJid = [m.sender]
   conn.reply(m.chat, tag, m, { contextInfo: { mentionedJid }})
 }
 handler.help = ['tagme']
 handler.tags = ['group']
 handler.command = /^tagme$/i
 
-handler.group = true
+handler.group = false
 
 module.exports = handler
