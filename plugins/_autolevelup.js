3e981015fdf5059a9576c33ac6aa2a709b8142fa
ryuzenxx <81517573+ryznxx@users.noreply.github.com>
2021-10-24 23:04:52 +0700

    Level Images

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,3 +1,7 @@
+let util = require('util')
+let path = require('path')
+let { spawn } = require('child_process')
+
 let handler = m => m
 let levelling = require('../lib/levelling')
 handler.before = m => {
@@ -7,14 +11,54 @@ handler.before = m => {
     let before = user.level * 1
     while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-        let str = `Selamat @${m.sender.split`@`[0]} Anda Naik 🧬level 
-*${before}* --> *${user.level}*
-`.trim()
-        conn.reply(m.chat, str, false, {
-            contextInfo: {
-                mentionedJid: [m.sender]
-            }
-        })
+        let str = `Selamat *${name}* naik level \n• Level Sebelumnya : ${before}\n• Level Baru : ${user.level}\n• Pada Jam : ${time}\n\n*_Semakin sering berinteraksi dengan bot Semakin Tinggi level kamu_*`.trim()
+        if (!global.support.convert && !global.support.magick && !global.support.gm) return handler.disabled = true
+        let fontLevel = 'src/level_c.otf'
+        let fontTexts = 'src/texts.otf'
+        let xtsx = 'src/lvlup_template.jpg'
+        let lvlnow = `${user.level}`
+        let teks = `Selamat anda naik level`
+        let bufs = []
+        const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
+	    'convert',
+	    xtsx,
+	    '-font',
+	    fontTexts, 
+	    '-fill', 
+	    '#0F3E6A', 
+	    '-size',
+	    '1024x784',
+	    '-pointsize',
+	    '68',
+	    '-interline-spacing',
+	    '-7.5',
+	    '-annotate',
+	    '+153+200',
+	    teks,
+	    //original together
+	    '-font',
+	    fontLevel, 
+	    '-fill', 
+	    '#0A2A48', 
+	    '-size',
+	    '1024x784',
+	    '-pointsize',
+	    '140',
+	    '-interline-spacing',
+	    '-1.2',
+	    '-annotate',
+	    '+1370+260',
+	    lvlnow,
+	    '-append',
+	    'jpg:-'
+	  ]
+	  spawn(_spawnprocess, _spawnargs)
+	    .on('error', e => conn.reply(m.chat, util.format(e), m))
+	    .on('close', () => {
+	      conn.sendFile(m.chat, Buffer.concat(bufs), 'result.jpg', str, m)
+	    })
+	    .stdout.on('data', chunk => bufs.push(chunk))
+	
     }
     return true
 }
