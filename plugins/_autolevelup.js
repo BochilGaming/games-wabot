3b8a9a876a492e27eebaf2376614903fd73874e4
ryuzenxx <81517573+ryznxx@users.noreply.github.com>
2021-10-25 18:53:09 +0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -17,7 +17,7 @@ module.exports = {
       })
       let name = this.getName(m.sender)
       let lvlnow = user.level
-      let teks = `Selamat *${name}* naik 🧬level`
+      let teks = `Selamat ${name} naik level` // teks di gambar tidak bisa ditambahkan emoticon
       let str = `
 ${teks} 
 
@@ -32,6 +32,19 @@ ${teks}
         let fontTexts = 'src/texts.otf'
         let xtsx = 'src/lvlup_template.jpg'
         let bufs = []
+        var anotations = '+1385+260' // gapake else if kadang error
+        if (lvlnow > 2) {
+  	var anotations = '+1370+260'
+        }
+        if (lvlnow > 10) {
+  	var anotations = '+1330+260'
+        } 
+        if (lvlnow > 50) {
+  	var anotations = '+1310+260'
+        } 
+        if (lvlnow > 100) {
+  	var anotations = '+1260+260'
+        } 
         const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
           'convert',
           xtsx,
@@ -60,7 +73,7 @@ ${teks}
           '-interline-spacing',
           '-1.2',
           '-annotate',
-          '+1370+260',
+          anotations,
           lvlnow,
           '-append',
           'jpg:-'
