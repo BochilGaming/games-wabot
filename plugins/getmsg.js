86a1833c88d1b84adc8e61ef0c149f4244c32514
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2021-10-31 06:38:37 +0000

    Add media cmd (#114)
    * add media cmd
    * add msg
    * simple
    
    Co-authored-by: BochilGaming <79433517+BochilGaming@users.noreply.github.com>

---
+++ plugins/getmsg.js
@@ -0,0 +1,25 @@
+let handler = async (m, { conn, command, usedPrefix, text }) => {
+    let which = command.replace(/get/i, '')
+    if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat list nya`
+    let msgs = global.DATABASE._data.msgs
+    if (!(text in msgs)) throw `'${text}' tidak terdaftar di list pesan`
+    let _m = conn.serializeM(JSON.parse(JSON.stringify(msgs[text]), (_, v) => {
+        if (
+            v !== null &&
+            typeof v === 'object' &&
+            'type' in v &&
+            v.type === 'Buffer' &&
+            'data' in v &&
+            Array.isArray(v.data)) {
+            return Buffer.from(v.data)
+        }
+        return v
+    }))
+    // m.reply(`[debug]\n${require('util').format(_m)}`)
+    await _m.copyNForward(m.chat, true)
+}
+handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'get' + v + ' <teks>')
+handler.tags = ['database']
+handler.command = /^get(vn|msg|video|audio|img|stic?ker|gif)$/
+
+module.exports = handler
---
+++ plugins/listmsg.js
@@ -0,0 +1,38 @@
+let handler = async (m, { usedPrefix, command }) => {
+    let which = command.replace(/(daftar|list)/i, '')
+    let msgs = global.DATABASE._data.msgs
+    let split = Object.entries(msgs).map(([nama, isi]) => { return { nama, ...isi } })
+    let fltr
+    if (/audio/i.test(command)) fltr = split
+        .filter(v => v.message.audioMessage)
+        .filter(m => m.message.audioMessage.ptt == false)
+        .map(v => '- ' + v.nama).join('\n')
+    if (/vn/i.test(command)) fltr = split
+        .filter(v => v.message.audioMessage)
+        .filter(m => m.message.audioMessage.ptt == true)
+        .map(v => '- ' + v.nama).join('\n')
+    if (/video/i.test(command)) fltr = split
+        .filter(v => v.message.videoMessage && !v.message.videoMessage.gifPlayback)
+        .map(v => '- ' + v.nama).join('\n')
+    if (/gif/i.test(command)) fltr = split
+        .filter(v => v.message.videoMessage)
+        .filter(m => m.message.videoMessage.gifPlayback)
+        .map(v => '- ' + v.nama).join('\n')
+    if (/stic?ker/i.test(command)) fltr = split.filter(v => v.message.stickerMessage).map(v => '- ' + v.nama).join('\n')
+    if (/msg/i.test(command)) fltr = split.filter(v => v.message.conversation).map(v => '- ' + v.nama).join('\n')
+    if (/img/i.test(command)) fltr = split.filter(v => v.message.imageMessage).map(v => '- ' + v.nama).join('\n')
+    m.reply(`
+*LIST PESAN*
+
+${fltr}
+
+Akses/ambil dengan mengetik:
+*${usedPrefix}get${which}* <nama>
+atau langsung ketik teksnya
+`.trim())
+}
+handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'list' + v)
+handler.tags = ['database']
+handler.command = /^(daftar|list)(vn|msg|video|audio|img|stic?ker|gif)$/
+
+module.exports = handler
