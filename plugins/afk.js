c9aa57ab000ce80fef53910c2443b01a35ed127e
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-11-04 23:05:55 +0800

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,10 +1,8 @@
-let handler = async (m, { text }) => {
+let handler = async (m, { conn, text }) => {
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`
-${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
-`)
+  await conn.sendButton(m.chat, `${conn.getName(m.sender)} is now AFK ${text ? ': ' + text : 'without reason'}`, 'Games-Wabot', 'DAH SIAP AFK:V', '.', m)
 }
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
