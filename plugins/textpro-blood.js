d0f334c3f264174eb5d9390f6fc2ee20abef11aa
botstylee <85060672+botstylee@users.noreply.github.com>
2021-11-14 12:08:29 +0700

    Mengscrape textpro dulu dari ghost #120

---
+++ plugins/textpro-blood.js
@@ -0,0 +1,50 @@
+const textpro = require("../lib/textpro.js");
+const axios = require ("axios")
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, args}) => {
+    let txt = args.join` `
+    	  if (!txt) return conn.reply(m.chat, 'harap masukan teksnya!!!', m)
+    	  if (txt.length > 15) return conn.reply (m.chat, 'maksimal 15',m)
+    textpro("https://textpro.me/blood-text-on-the-frosted-glass-941.html", [`${txt}`])
+.then(async (data) => {
+let au = `${data}`
+console.log(data)
+let buf = await getBuffer(data);
+conn.reply(m.chat, '_tunggu sebentar. . ._',m)
+conn.sendMessage(m.chat, buf, MessageType.image, { quoted: m, caption: 'nihhhh!!!!'} )
+})
+.catch(error => console.log(error));
+}
+handler.help = ['blood <text>']
+handler.tags = ['textpro']
+handler.command = /^blood$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+handler.limit = true
+module.exports = handler
+
+const getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+                    'User-Agent': 'GoogleBot',
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
+}
