9ef184d969a8ecc5b0d36b85c29b376d37fc9862
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-11-14 19:05:00 +0700

    ...

--- plugins/sgura.js
+++ plugins/sgura.js
@@ -5,10 +5,9 @@ let handler = async (m, { conn, text }) => {
   if (!text) text = await conn.getName(m.sender)
   await m.reply(global.wait)
   let namaguwe = conn.getName(m.sender)
-  let apiUrl = global.API( 'http://hardianto-chan.herokuapp.com', '/api/bot/gura', {
-    apikey: 'hardianto',
+  let apiUrl = global.API('hardianto', '/api/bot/gura', {
     nama: text
-  })
+  }, 'apikey')
 try {
     let stiker = await sticker(null, apiUrl, 'Guraa', namaguwe)
     await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
@@ -22,7 +21,7 @@ try {
 handler.help = ['sgura <teks>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?gura$/i
-handler.register = true
+
 handler.limit = true
 
 module.exports = handler
