b42b7c8b09882b40033896f1faa11ae7c64b7491
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-11-22 13:00:10 +0800

    uptime:v

---
+++ plugins/uptime.js
@@ -0,0 +1,24 @@
+let handler = async (m, { conn }) => {
+    let _uptime = process.uptime() * 1000
+    let uptime = waktu(_uptime)
+
+m.reply(`Aktif selama ${uptime}`.trim())
+}
+handler.help = ['uptime']
+handler.tags = ['info']
+handler.command = /^(uptime)$/i
+
+module.exports = handler
+
+function waktu(seconds) {
+	seconds = Number(seconds);
+	var d = Math.floor(seconds / (3600 * 24));
+	var h = Math.floor(seconds % (3600 * 24) / 3600);
+	var m = Math.floor(seconds % 3600 / 60);
+	var s = Math.floor(seconds % 60);
+	var dDisplay = d > 0 ? d + (d == 1 ? " hari, " : " hari, ") : "";
+	var hDisplay = h > 0 ? h + (h == 1 ? " jam, " : " jam, ") : "";
+	var mDisplay = m > 0 ? m + (m == 1 ? " menit, " : " menit, ") : "";
+	var sDisplay = s > 0 ? s + (s == 1 ? " detik" : " detik") : "";
+	return dDisplay + hDisplay + mDisplay + sDisplay;
+}
