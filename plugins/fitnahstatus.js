2b67d768fb268a5ad4489832529c484bea3866ea
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-11-28 15:38:23 +0800

    fitnah status??? (#136)
    * fitnah status???

---
+++ plugins/fitnahstatus.js
@@ -0,0 +1,23 @@
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, text }) => {
+  if (!text) return
+  let cm = copy(m)
+  let who
+  if (m.isGroup) who = cm.participant = m.mentionedJid[0]
+  else who = m.chat
+  if (!who) throw 'Tag salah satu lah'
+  cm.key.fromMe = false
+  cm.message[m.mtype] = copy(m.msg)
+  let sp = '@' + who.split`@`[0]
+  let [fake, ...real] = text.split(sp)
+  conn.fakeReply(m.chat, real.join(sp).trimStart(), who, fake.trimEnd(), 'status@broadcast'/*, { contextInfo: {
+    mentionedJid: [conn.parseMention(real.join(sp))]
+  }}*/)
+}
+handler.command = /^(fitnahstatus)$/
+
+module.exports = handler
+
+function copy(obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
