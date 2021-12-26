744d4380184375ff6c1aa48127f5d50cfc79bdc2
zatu22 <74061098+zatu22@users.noreply.github.com>
2021-12-26 11:39:17 +0100

    Bug fix
    -Added missing node modules & fix
    -Fixed print message
    -Fixed global.owner error
    - New entry gitignore
    =MultiDevice is working now

--- config.js
+++ config.js
@@ -1,28 +1,6 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
-global.Owner = {
-  // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '6281390658325': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
-    isDev: true, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: true // if true this number will send if user use command /owner or /creator
-  },
-  '6282256115584': { // put your number here
-    name: 'Dave',
-    isDev: false, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
-  },
-  '60199782326': {
-    name: 'Adiixyz',
-    isReport: true, // if true this number will send if user use command /report or /join
-  },
-  '6285713964963': {
-    isReport: true
-  },
-  '13479805233': {} // ignore if you don't want anything, like name dll
-}
+global.owner = ['6281390658325', '6282256115584', '60199782326', '6285713964963','13479805233']
 global.mods = ['13479805233'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
