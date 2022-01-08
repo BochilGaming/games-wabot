81daa5fb3869aa04ed2803bcf91354337e446c6f
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-01-08 17:35:21 +0700

    Fix menu double

--- plugins/menu.js
+++ plugins/menu.js
@@ -144,12 +144,11 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     let pp = await conn.getProfilePicture(conn.user.jid).catch(_ => path.join(__dirname, '../src/avatar_contact.png'))
-    conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m).catch(_ => conn.reply(m.chat, text.trim(), m))
-conn.sendButton(m.chat,text.trim(), author,  pp,  [
+    conn.sendButton(m.chat,text.trim(), author,  pp,  [
   ['Ping',  '/ping'],
   ['Info',  '/info'],
   ['Owner',  '/owner']
-], { quoted: m})
+], { quoted: m}).catch(_ => conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m)).catch(_ => conn.reply(m.chat, text.trim(), m))
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
