a22dc7f1dd4180ff29d5bfe0339f4b6c4fe8d49b
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-01-10 13:20:34 +0700

    Fix creator if not have name

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   let contacts = []
   for (let owner of Object.entries(global.Owner).filter(v => v[1].isCreator)) {
-    contacts.push(...[owner[0], owner[1].name])
+    contacts.push(...[owner[0], (owner[1].name || conn.getName(owner[0]) || '')])
   }
   if (contacts.length < 3) return this.sendContact(m.chat, contacts[0], contacts[1], m)
   this.sendContactArray(m.chat, contacts, m)
