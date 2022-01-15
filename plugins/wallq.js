1552180ef2d0e802a1c14879b4695fc000dff21f
unxzx <84166927+unx21@users.noreply.github.com>
2022-01-15 18:21:50 +0800

    Tambahin tombol

--- plugins/wallq.js
+++ plugins/wallq.js
@@ -1,10 +1,8 @@
-// cewe yang ada di iklan royco bikin ange njing
-// pdhl cuma iklan :v
-
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'Nyari apa?'
+  await m.reply(global.wait)
   let res = await fetch(global.API('https://wall.alphacoders.com/api2.0','/get.php', {
     auth: '3e7756c85df54b78f934a284c11abe4e',
     method: 'search',
@@ -13,11 +11,11 @@ let handler = async (m, { conn, text }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
-  await conn.sendFile(m.chat, img.url_image, 'wallpaper', 'Nih wallpaper!', m)
+  await conn.sendButtonImg(m.chat, await(await fetch(img.url_image)).buffer(), `Hasil Pencarian\n${text}`, '© Metro-Bot', 'Next', m.text, m)
 }
 handler.help = ['wallpaperq <query>']
 handler.tags = ['internet']
 handler.command = /^wall(paper)?q?$/i
-handler.limit = true
+handler.register = false
 
 module.exports = handler
