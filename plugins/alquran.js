39c90511a603e6c80dd6652ebde6527e74a4da35
Frasydi <97487531+Frasydi@users.noreply.github.com>
2022-01-17 02:48:01 +0800

    Audio dan Text dipisah
    Sebuah tombol akan ditampilkan untuk memunculkan audio ataukah tidak

--- plugins/alquran.js
+++ plugins/alquran.js
@@ -1,53 +1,60 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!(args[0] || args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nmaka hasilnya adalah surah Al-Fatihah ayat 2`
-    if (isNaN(args[0]) || isNaN(args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 `
-    let res = await alquran(args[0], args[1])
-    m.reply(`
-${res.arab}
-${res.latin}
-
-${res.terjemahan}
-${readMore}
-${res.tafsir}
-
-( ${res.surah} )
-`.trim())
-    conn.sendFile(m.chat, res.audio, 'audio.mp3', '', m)
+    let prehelp = `${usedPrefix + command}`
+    if (sudahbaca === false) {
+    let res = await alquran(args[0], args[1], prehelp)
+    return await conn.sendButton(m.chat, `${res.pesan}`.trim(), '© stikerin', 'Menampilkan audio', `${usedPrefix+command} audio`)
+    } else {
+        if (args[0] === "audio") {
+            if (sudahbaca === true) {
+                conn.sendFile(m.chat, audioa, 'Alquran.mp3', '', m)
+            }
+            sudahbaca = false;
+        }
+    }
 }
+
+
 handler.help = ['alquran <114> <1>']
 handler.tags = ['quran']
 handler.command = /^(al)?quran$/i
 module.exports = handler
-
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-
+global.sudahbaca = false;
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
-async function alquran(surah, ayat) {
+async function alquran2(ad) {
+audioa
+}
+async function alquran(surah, ayat, hlp) {
+    if (!(surah||ayat)) return `contoh:\n${hlp} 1 2 \n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 `
+    if (isNaN(surah)||isNaN(ayat)) return `contoh:\n${hlp} 1 2 \n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 `
     let res = await fetch(`https://kalam.sindonews.com/ayat/${ayat}/${surah}`)
     if (!res.ok) throw 'Error, maybe not found?'
     let $ = cheerio.load(await res.text())
-    let content = $('body > main > div > div.content.clearfix > div.news > section > div.list-content.clearfix')
-    let Surah = $(content).find('div.ayat-title > h1').text()
-    let arab = $(content).find('div.ayat-detail > div.ayat-arab').text()
-    let latin = $(content).find('div.ayat-detail > div.ayat-latin').text()
-    let terjemahan = $(content).find('div.ayat-detail > div.ayat-detail-text').text()
+    let content = $('body > main > div > div.content.clearfix > div.news > section > div.list-content.clearfix')//content
+    let Surah = $(content).find('div.ayat-title > h1').text()//surah
+    let arab = $(content).find('div.ayat-detail > div.ayat-arab').text()//arab
+    let latin = $(content).find('div.ayat-detail > div.ayat-latin').text()//latin
+    let terjemahan = $(content).find('div.ayat-detail > div.ayat-detail-text').text()//terjemahan
     let tafsir = ''
     $(content).find('div.ayat-detail > div.tafsir-box > div').each(function () {
         tafsir += $(this).text() + '\n'
-    })
+    })//tafsir
     tafsir = tafsir.trim()
-    let keterangan = $(content).find('div.ayat-detail > div.ayat-summary').text()
+    keterangan = $(content).find('div.ayat-detail > div.ayat-summary').text()//keterangan
+    let isi = []
+    isi.push(arab, latin, terjemahan, readMore, tafsir, "(" + Surah + ")")
     // https://quran.kemenag.go.id/assets/js/quran.js
-    let audio = `https://quran.kemenag.go.id/cmsq/source/s01/${surah < 10 ? '00' : surah >= 10 && surah < 100 ? '0' : ''}${surah}${ayat < 10 ? '00' : ayat >= 10 && ayat < 100 ? '0' : ''}${ayat}.mp3`
+    audioa = `https://quran.kemenag.go.id/cmsq/source/s01/${surah < 10 ? '00' : surah >= 10 && surah < 100 ? '0' : ''}${surah}${ayat < 10 ? '00' : ayat >= 10 && ayat < 100 ? '0' : ''}${ayat}.mp3`
+    pesan = ""
+    for (let i = 0; i < isi.length;i++) {
+        pesan += isi[i]
+        pesan += "\n\n"
+    }
+    sudahbaca = true;
     return {
-        surah: Surah,
-        arab,
-        latin,
-        terjemahan,
-        tafsir,
-        audio,
-        keterangan,
+        pesan,
+        keterangan
     }
 }
