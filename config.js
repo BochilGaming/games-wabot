97af921903b4954e3c25512480444208293df16a
BlueShiYT <81657050+BlueShiYT@users.noreply.github.com>
2022-01-18 09:18:11 +0700

    Add Leyscoder api (#176)
    * Add Leyscoder api
    
    Biar fitur pp couple nya work banh
    
    * Fix banh tdi lupa kasih koma:v

--- config.js
+++ config.js
@@ -37,7 +37,8 @@ global.APIs = { // API Prefix
   zahir: 'https://zahirr-web.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
   pencarikode: 'https://pencarikode.xyz',
-  hardianto: 'http://hardianto-chan.herokuapp.com'
+  hardianto: 'http://hardianto-chan.herokuapp.com',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
@@ -45,7 +46,8 @@ global.APIKeys = { // APIKey Here
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais',
-  'http://hardianto-chan.herokuapp.com': 'hardianto'
+  'http://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
 // Sticker WM
