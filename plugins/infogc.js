f528dc74b88d59e583f229b6b0f96a42d683c168
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-01-19 09:08:47 +0700

    Update infogc.js

--- plugins/infogc.js
+++ plugins/infogc.js
@@ -1,20 +1,12 @@
 let handler = async (m, { conn, participants, groupMetadata }) => {
-
-    const getGroupAdmins = (participants) => {
-        admins = []
-        for (let i of participants) {
-            i.isAdmin ? admins.push(i.jid) : ''
-        }
-        return admins
-    }
-
     let pp = './src/avatar_contact.png'
     try {
         pp = await conn.getProfilePicture(m.chat)
     } catch (e) {
     } finally {
-        let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink } = global.DATABASE._data.chats[m.chat]
-        const groupAdmins = getGroupAdmins(participants)
+        let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: Delete } = global.DATABASE._data.chats[m.chat]
+        const groupAdmins = participants.filter(i => i.isAdmin)
+        const groupOwner = groupMetadata.owner || `${m.chat.split`-`[0]}@s.whatsapp.net`
         let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
         let text = `*「 Group Information 」*\n
 *ID:* 
@@ -26,14 +18,14 @@ ${groupMetadata.desc}
 *Total Members:*
 ${participants.length} Members
 *Group Owner:* 
-@${m.chat.split`-`[0]}
+@${groupOwner.split('@')[0]}
 *Group Admins:*
 ${listAdmin}
 *Group Settings:*
 ${isBanned ? '✅' : '❌'} Banned
 ${welcome ? '✅' : '❌'} Welcome
 ${detect ? '✅' : '❌'} Detect
-${global.DATABASE._data.chats[m.chat].delete ? '❌' : '✅'} Anti Delete
+${Delete ? '❌' : '✅'} Anti Delete
 ${antiLink ? '✅' : '❌'} Anti Link
 *Message Settings:*
 Welcome: ${sWelcome}
@@ -41,9 +33,12 @@ Bye: ${sBye}
 Promote: ${sPromote}
 Demote: ${sDemote}
 `.trim()
-        ownernya = [`${m.chat.split`-`[0]}@s.whatsapp.net`]
-        let mentionedJid = groupAdmins.concat(ownernya)
-        conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, { contextInfo: { mentionedJid } })
+        let mentionedJid = groupAdmins.concat([groupOwner])
+        conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, {
+            contextInfo: {
+                mentionedJid
+            }
+        })
     }
 }
 handler.help = ['infogrup']
