0f42093a8ba555f511046ff3a5815f7183759023
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-01-20 10:50:37 +0700

    no mater if plugins not in global.plugins

--- plugins/getplugin.js
+++ plugins/getplugin.js
@@ -1,11 +1,17 @@
+const fs = require('fs')
+const path = require('path')
 let handler = async (m, { usedPrefix, command, text }) => {
-    let ar = Object.keys(plugins)
-    let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!text) throw `where is da text?\n\nexempel: ${usedPrefix + command} waifu`
-    if (!ar1.includes(text)) return m.reply(`'${text}' not found!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
-    m.reply(require('fs').readFileSync('./plugins/' + text + '.js', 'utf-8'))
+    if (!text) throw `where is the text?\n\nexempel: ${usedPrefix + command} menu`
+    const filename = path.join(__dirname, `./${text}${!/\.js$/i.test(text) ? '.js' : ''}`)
+    const listPlugins = fs.readdirSync(path.join(__dirname)).map(v => v.replace(/\.js/, ''))
+    if (!fs.existsSync(filename)) return m.reply(`
+'${filename}' not found!
+
+${listPlugins.map(v => v).join('\n').trim()}
+`.trim())
+    m.reply(fs.readFileSync(filename, 'utf8'))
 }
-handler.help = ['getplugin'].map(v => v + ' [teks]')
+handler.help = ['getplugin'].map(v => v + ' [filename]')
 handler.tags = ['owner']
 handler.command = /^(getplugin|get ?plugin)$/i
 
