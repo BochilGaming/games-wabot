872c67e4428bc9a109a5961bc69bdcf82ca43444
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-01-21 08:52:27 +0700

    Fix bot utama

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,11 +1,8 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply(`🌟 *Bot Utama* \n\n• https://wa.me/6285713964963?text=${usedPrefix}menu (${conn.user.name}) \n\n *🤖JadiBot* \n\n` + users.map(v => '• https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+  m.reply(`🌟 *Bot Utama* \n\n• https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🤖JadiBot* \n\n` + users.map(v => '• https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
 handler.command = handler.help = ['listjadibot']
 handler.tags = ['jadibot']
 
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
