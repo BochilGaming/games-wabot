f7de3201c74713dea93902b11cb22afcaf8c88ff
BlueShiYT <81657050+BlueShiYT@users.noreply.github.com>
2022-01-21 08:54:04 +0700

    Create listjadibot.js (#184)
    * Create listjadibot.js
    
    * Fix bot utama
    
    Co-authored-by: BochilGaming <79433517+BochilGaming@users.noreply.github.com>

---
+++ plugins/listjadibot.js
@@ -0,0 +1,8 @@
+async function handler(m, { usedPrefix }) {
+  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
+  m.reply(`🌟 *Bot Utama* \n\n• https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🤖JadiBot* \n\n` + users.map(v => '• https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+}
+handler.command = handler.help = ['listjadibot']
+handler.tags = ['jadibot']
+
+module.exports = handler
