ab3f4239f63675b654442d70af18334f8e1e0890
BlueShiYT <81657050+BlueShiYT@users.noreply.github.com>
2022-01-31 13:25:02 +0700

    Create cooldown.js (#202)
    * Create cooldown.js
    
    Buttonnya masih bug
    
    * Fix sendButton and cooldown!
    
    * remove unused variable
    
    Co-authored-by: BochilGaming <79433517+BochilGaming@users.noreply.github.com>

--- plugins/monthly.js
+++ plugins/monthly.js
@@ -1,10 +1,10 @@
 let { MessageType } = require('@adiwajshing/baileys')
-
+const cooldown = 2592000000
 let handler = async (m, { conn }) => {
     let user = global.DATABASE._data.users[m.sender]
-    let _timers = (2592000000 - (new Date - user.lastmonthly))
-    let timers = clockString(_timers) 
-    if (new Date - user.lastmonthly > 2592000000) {
+    let _timers = (cooldown - (new Date - user.lastmonthly))
+    let timers = clockString(_timers)
+    if (new Date - user.lastmonthly > cooldown) {
         conn.reply(m.chat, `Anda sudah mengklaim dan mendapatkan 100000 💵money, 5 🎁Legendary crate dan 3 📦Pet crate`, m)
         user.money += 100000
         user.legendary += 5
@@ -19,40 +19,37 @@ handler.help = ['monthly']
 handler.tags = ['rpg']
 handler.command = /^(monthly)$/i
 
-handler.fail = null
+handler.cooldown = cooldown
 
 module.exports = handler
 
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
 function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
-  let m = Math.floor(ms / 60000) % 60
-  let s = Math.floor(ms / 1000) % 60
-  console.log({ms,h,m,s})
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+    let h = Math.floor(ms / 3600000)
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    console.log({ ms, h, m, s })
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
 function button(teks, user) {
     const buttons = []
-    
+
     let claim = new Date - user.lastclaim > 86400000
     let monthly = new Date - user.lastmonthly > 2592000000
     let weekly = new Date - user.lastweekly > 604800000
-    console.log({claim, monthly, weekly})
-    
-    if (monthly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/monthly'}, type: 1})
-    if (weekly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/weekly'}, type: 1})
-    if (claim) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/claim'}, type: 1})
+    console.log({ claim, monthly, weekly })
+
+    if (monthly) buttons.push({ buttonId: `id${buttons.length + 1}`, buttonText: { displayText: '/monthly' }, type: 1 })
+    if (weekly) buttons.push({ buttonId: `id${buttons.length + 1}`, buttonText: { displayText: '/weekly' }, type: 1 })
+    if (claim) buttons.push({ buttonId: `id${buttons.length + 1}`, buttonText: { displayText: '/claim' }, type: 1 })
     if (buttons.length == 0) throw teks
-    
+
     const buttonMessage = {
         contentText: teks,
         footerText: '©games-wabot',
         buttons: buttons,
         headerType: 1
     }
-    
+
     return buttonMessage
 }
