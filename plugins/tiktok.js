fbb4c578968a122cdc886781b64622faa9f489c7
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-02-01 15:51:58 +0700

    Fix tiktok?
    maybe fix https://github.com/BochilGaming/games-wabot/issues/204#issuecomment-1026499925

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,13 +1,12 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args, command, usedPrefix }) => {
-  if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} https://tiktokxxxx`
-  let { video, description, music, author } = await tiktok(args[0])
-  let url = video.no_watermark || video.with_watermark || video.no_watermark_raw || music
-  if (!url) throw 'Gagal mengambil url download'
-  await conn.sendFile(m.chat, url, 'tiktok.mp4', `
-- *By:* ${author.nickname} (${author.unique_id})
-- *Desc:*
-${description}
+const { tiktokdl, tiktokdlv2 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
+    const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+🔗 *Url:* ${url}
+🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
 `.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
@@ -16,33 +15,3 @@ handler.tags = ['downloader']
 handler.command = /^(tik(tok)?(dl)?)$/i
 
 module.exports = handler
-
-const axios = require('axios')
-async function tiktok(url) {
-  try {
-    let results = {}
-    if (/v[tm]\.tiktok\.com/g.test(url)) {
-      let res = await axios.get(url)
-      url = res.request.res.responseUrl
-    }
-    let key = await axios.get(`https://api.snaptik.site/video-key?video_url=${url}`)
-    key = JSON.parse(JSON.stringify(key.data, null, 2))
-    if (key.status !== 'success') throw key
-    let data = await axios.get(`https://api.snaptik.site/video-details-by-key?key=${key.data.key}`)
-    data = JSON.parse(JSON.stringify(data.data, null, 2))
-    if (data.status !== 'success') throw data
-    results = {
-      author: { ...data.data.author },
-      description: data.data.description,
-      video: {
-        with_watermark: `https://api.snaptik.site/download?key=${data.data.video.with_watermark}&type=video`,
-        no_watermark: `https://api.snaptik.site/download?key=${data.data.video.no_watermark}&type=video`,
-        no_watermark_raw: data.data.video.no_watermark_raw
-      },
-      music: `https://api.snaptik.site/download?key=${data.data.music}&type=music`
-    }
-    return results
-  } catch (e) {
-    throw 'Maybe video private?!'
-  }
-}
