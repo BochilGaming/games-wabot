38738aa34e1443370eecce3cdaed5b0322652953
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-02-01 07:49:20 +0700

    Update cosplay.js

--- plugins/cosplay.js
+++ plugins/cosplay.js
@@ -1,7 +1,7 @@
 // wahai para para weaboo🗿
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, `https://adiixyzapi.herokuapp.com/api/randomimage/cosplay`, 'cosplay.jpg', '_*Done*_', m)
+  conn.sendFile(m.chat, global.API('adiisus', '/api/randomimage/cosplay'), 'cosplay.jpg', '_*Done*_', m)
 }
 handler.help = ['cosplay']
 handler.tags = ['internet']
