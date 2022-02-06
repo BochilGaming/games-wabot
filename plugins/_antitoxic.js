e7928d2220321d094d23f5650efe2531ed3ee9b8
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-07 00:26:02 +0530

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -3,12 +3,12 @@ let handler = m => m
 let linkRegex = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
 handler.before = function (m, { user }) {
   if (m.isBaileys && m.fromMe) return true
-  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
+  if (/login|continue|flood|(per)?length/g.exec(m.text)) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupToxic = linkRegex.exec(m.text)
 
   if (chat.antiToxic && isGroupToxic) {
-    m.reply('Do not Toxic yes!!\n' + readMore + '\nMau Turn off? Type */disable antitoxic*')
+    m.reply('Do not Toxic yes!!\n' + readMore + '\nwant to Turn off? Type */disable antitoxic*')
     if (global.opts['restrict']) {
       // if (!user.isAdmin) return true
       // this.groupRemove(m.chat, [m.sender])
