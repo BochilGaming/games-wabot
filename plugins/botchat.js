8676942b1d512177ea41240820859c8e9c4097f9
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-07 01:29:43 +0530

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
 let name = conn.getName(m.sender)
 let teks = `
-${pickRandom([` _Hii @${m.sender.split`@`[0]}_\n\n_Saya disini_`, `_Oeetss @${m.sender.split`@`[0]}_\n\n_Ada apa??_`, `_Ekhheemmm, Uhuk Ughhuk_`, `_Puuuiyy_`, `_Poom_`, `_Hadirrr_`, `_Yooossshhhh_`])}
+${pickRandom([` _Hey @${m.sender.split`@`[0]}_\n\n_I'm here_`, `_Oeetss @${m.sender.split`@`[0]}_\n\n_What's up??_  `, `_Ekhheemmm, Cough Ughhuk_`, `_Puuuiyy_`, `_Poom_`, `_Hairrr_`, `_Yoossshhhh_`])}
 `.trim()
 conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [m.sender] }})
 }
