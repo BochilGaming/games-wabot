04a2eab54c6908e4f01c862a409a485c15f7edbf
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-07 02:08:43 +0530

    Update family100_answer.js

--- plugins/family100_answer.js
+++ plugins/family100_answer.js
@@ -9,29 +9,29 @@ module.exports = {
         let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (!isSurrender) {
-            let index = room.jawaban.indexOf(text)
+            let index = room.answer.indexOf(text)
             if (index < 0) {
-                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('Dikit lagi!')
+                if (Math.max(...room.answer.filter((_, index) => !room.answered[index]).map(answer => similarity(answer, text))) >= threshold) m.reply('A little more!')
                 return !0
             }
-            if (room.terjawab[index]) return !0
+            if (room.answered[index]) return !0
             let users = global.DATABASE._data.users[m.sender]
             room.terjawab[index] = m.sender
             users.exp += room.winScore
         }
-        let isWin = room.terjawab.length === room.terjawab.filter(v => v).length
+        let isWin = room.terjawab.length === room.answered.filter(v => v).length
         let caption = `
 *Soal:* ${room.soal}
 
-Terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
-(beberapa jawaban terdapat spasi)
+There is *${room.answer.length}* answer${room.answer.find(v => v.includes(' ')) ? `
+(some answers have spaces)
 `: ''}
-${isWin ? `*SEMUA JAWABAN TERJAWAB*` : isSurrender ? '*MENYERAH!*' : ''}
-${Array.from(room.jawaban, (jawaban, index) => {
-            return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
+${isWin ? `*ALL ANSWERS*` : isSurrender ? '*SURRENDER!*' : ''}
+${Array.from(room.answer, (answer, index) => {
+            return isSurrender || room.answered[index] ? `(${index + 1}) ${answer} ${room.teranswer[index] ? '@' + room.answered[index].split('@')[0] : ''}`.trim() : false
         }).filter(v => v).join('\n')}
 
-${isSurrender ? '' : `+${room.winScore} XP tiap jawaban benar`}
+${isSurrender ? '' : `+${room.winScore} XP every answer is correct`}
     `.trim()
         m.reply(caption, null, {
             contextInfo: {
