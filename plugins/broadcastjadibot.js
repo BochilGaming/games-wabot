7c662eba46441c7444601e94f1ef5b1a8b1f23e5
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-07 00:43:53 +0530

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text }) => {
   if (conn.user.jid === global.conn.user.jid) {
     let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
-    let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Broadcast 」')
+    let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' 𝘽𝙧𝙤𝙖𝙙𝙘𝙖𝙨𝙩 」')
     for (let id of users) conn.copyNForward(id, content)
-    conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+    conn.reply(m.chat, `_Successfully broadcast to ${users.length} the bot number_
 ${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g,'') + '?text=.menu').join('\n')}`.trim(), m)
-  } else conn.reply(m.chat, 'Fitur ini hanya untuk host bot',  m)
+  } else conn.reply(m.chat, 'This feature is only for bot hosts',  m)
 }
 handler.help = ['broadcastjadibot','bcbot'].map(v => v + ' <teks>')
 handler.tags = ['host']
