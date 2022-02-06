4edca0984933c80e7647e9a55d308cffd7555706
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2022-02-06 09:05:30 +0700

    loli pake pinterest (#208)
    * loli pake pinterest
    
    Siapa disini yang pedo adik"
    
    * Milf wangy
    
    Siapa disini pecinta emak"

---
+++ plugins/milf.js
@@ -0,0 +1,10 @@
+// wahai para para weaboo🗿
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, global.API('adiisus', '/api/randomimage/milf'), 'milf.jpg', '_*Nih Wibu*_', m)
+}
+handler.help = ['milf']
+handler.tags = ['internet']
+handler.command = /^(milf)$/i
+handler.limit = true
+module.exports = handler
