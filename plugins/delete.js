0fa3ac05c31770655da5a6626896d379cf3345f7
Toshiro  Hitsugaya <91844428+ToshiroHitsugaya223@users.noreply.github.com>
2022-02-07 00:18:51 +0530

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -2,7 +2,7 @@ let handler = function (m) {
   if (!m.quoted) throw false
   let { chat, fromMe, id, isBaileys } = m.quoted
   if (!fromMe) throw false
-  if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+  if (!isBaileys) throw 'The message was not sent by a bot!'
   this.deleteMessage(chat, {
     fromMe,
     id,
