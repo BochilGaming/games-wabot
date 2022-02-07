e317b9754c3d0c2780cdde3392b73083d520ad94
Adii <82200440+Adiixyz@users.noreply.github.com>
2022-02-07 13:50:33 +0800

    Update savefile.js

--- plugins/savefile.js
+++ plugins/savefile.js
@@ -1,9 +1,9 @@
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `where is the code?\n\nexample:\n${usedPrefix + command} plugins/menu.js`
+    if (!text) throw `where is the path?\n\nexample:\n${usedPrefix + command} plugins/menu.js`
     if (!m.quoted.text) throw `reply code`
     let path = `${text}`
     await require('fs').writeFileSync(path, m.quoted.text)
-    m.reply(`Saved, filename is ${path}`)
+    m.reply(`Saved ${path} to file!`)
 }
 handler.help = ['savefile', 'sf'].map(v => v + ' <path>')
 handler.tags = ['owner']
