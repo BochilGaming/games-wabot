b2c55e44ba52f80dfcc27727cce3fdd6da2107e5
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2022-02-09 08:09:20 +0700

    Kisah Nabi! (#211)
    Stay Halal Brother

---
+++ plugins/kisahnabi.js
@@ -0,0 +1,21 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text }) => {
+  if (!text) throw `Masukkan nama Nabi!\n\nContoh : #kisahnabi adam`
+  let res = await fetch(global.API('LeysCoder', '/api/nabi', { q: text }, 'apikey'))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if(!json.result) throw json
+  let { nabi, lahir, umur, tempat, image, kisah } = json.result
+let kisahnabi = `
+*Nama :* ${nabi}
+*Lahir :* ${lahir}
+*Umur :* ${umur}
+*Tempat :* ${tempat}
+*Kisah :* ${kisah}`
+
+  await conn.sendFile(m.chat, image, '', kisahnabi, m)
+}
+handler.help = ['kisahnabi <nama>']
+handler.tags = ['internet']
+handler.command = /^(kisahnabi)$/i
+module.exports = handler
