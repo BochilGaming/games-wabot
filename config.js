f23376fa7bc3ab4366ae55327e08a775e8da14d4
Araaacakep <100002720+Araaacakep@users.noreply.github.com>
2022-02-19 08:08:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/GXXFRSUgXbjLrM1Mfge59h', 'https://chat.whatsapp.com/HcGcIB09sIvKrGytO8yfFn']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '85249231619': { // put your number here
+    name: 'Owner Tata',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
@@ -52,8 +52,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/85249231619'
+global.author = 'Tata '
 
 global.multiplier = 39 // The higher, The harder levelup
 
