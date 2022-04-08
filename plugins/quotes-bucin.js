f93a8a94837ffc5cca8545a2daacfef3c3dc4a6d
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-08 16:25:16 +0700

    Update quotes-bucin.js

--- plugins/quotes-bucin.js
+++ plugins/quotes-bucin.js
@@ -1,6 +1,6 @@
 import { bucin } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, usedPrefix, command }) => conn.sendButton(m.chat, await bucin(), wm, [
+let handler = async (m, { conn, usedPrefix, command }) => conn.sendButton(m.chat, await bucin(), author, [
     ['Next', `${usedPrefix+command}`], 
 ], m)
 
