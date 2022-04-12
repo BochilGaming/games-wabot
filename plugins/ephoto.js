eac9cb25a75431baa1278be652fd59acaa38813e
FrierenDv <38551757+xct007@users.noreply.github.com>
2022-04-13 04:12:50 +0700

    Create ephoto.js

---
+++ plugins/ephoto.js
@@ -0,0 +1,41 @@
+// try
+
+import fetch from 'node-fetch'
+
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+
+
+//
+    let q = m.quoted ? m.quoted : m
+
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+
+    if (!mime) throw `Reply Foto/Kirim foto`
+
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+
+    let msg = await conn.sendMessage(m.chat, {text: "Prosess kak.."}, m)
+
+    let img = await q.download?.()
+
+    let upld = await uploadImage(img)
+
+    try {
+
+        let img = await fetch(global.API('xcoders', '/api/ephoto/' + args[0], { url: upld }, 'apikey')).then(a => a.buffer())
+
+        conn.sendFile(m.chat, img, 'tes.jpg', 'tes', m)
+
+    } catch (err) {
+
+        m.reply(err)
+
+    }
+
+}
+// handler.tags = 
+handler.command = /^vhs|cyberpunk$/i
+
+export default handler
