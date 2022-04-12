316c898228124b6e54a9d1a882ccbaa3709f063e
FrierenDv <38551757+xct007@users.noreply.github.com>
2022-04-13 03:59:27 +0700

    Update config.js
    Add api xcoders

--- config.js
+++ config.js
@@ -18,7 +18,8 @@ global.APIs = { // API Prefix
   zahir: 'https://zahirr-web.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
   pencarikode: 'https://pencarikode.xyz',
-  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  xcoders: 'https://api-xcoders.xyz'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
@@ -26,7 +27,8 @@ global.APIKeys = { // APIKey Here
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais',
-  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://api-xcoders.xyz': 'your apikey'
 }
 
 // Sticker WM
