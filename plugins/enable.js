578d89bc96256ad439eda1ab8a1f3acc09134310
FrierenDv <38551757+xct007@users.noreply.github.com>
2022-04-13 16:29:38 +0700

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -163,9 +163,18 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       }
       global.opts['swonly'] = isEnable
       break
+    case 'simi':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.simi = isEnable
+      break
     default:
       if (!/[01]/.test(command)) return m.reply(`
-List option: welcome | delete | public | antilink | antidelete | antitoxic | autolevelup | detect | document | whitelistmycontacts | restrict | nyimak | autoread | pconly | gconly | swonly
+List option: welcome | delete | public | antilink | antidelete | antitoxic | autolevelup | detect | document | whitelistmycontacts | restrict | nyimak | autoread | pconly | gconly | swonly | simi
 Contoh:
 ${usedPrefix}enable welcome
 ${usedPrefix}disable welcome
