cb45f4796a4888189dbc73a8e6cbbc3ebadcf159
FrierenDv <38551757+xct007@users.noreply.github.com>
2022-04-13 16:32:17 +0700

    Create _simi.js

---
+++ plugins/_simi.js
@@ -0,0 +1,23 @@
+// simi
+import fetch from 'node-fetch'
+
+export async function before(m) {
+    let chat = db.data.chats[m.chat]
+    if (chat.simi) {
+        if (!m.text) return
+        let url = `https://api-sv2.simsimi.net/v2/?text=${m.text}&lc=id&cf=true`
+        let res = await fetch(url)
+        let json = await res.json()
+        let {
+            response,
+            result
+        } = json.messages[0]
+        await this.sendPresenceUpdate('composing', m.chat)
+        await delay(2000)
+        m.reply(response)
+        return !0
+    }
+    return !0
+}
+
+const delay = time => new Promise(res => setTimeout(res, time))
