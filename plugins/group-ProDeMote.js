2bf983a065ae50f1bddc997a3e5bb9f5ed0842c9
FrierenDv <38551757+xct007@users.noreply.github.com>
2022-04-17 05:56:13 +0700

    Update group-ProDeMote.js

--- plugins/group-ProDeMote.js
+++ plugins/group-ProDeMote.js
@@ -1,13 +1,19 @@
 import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, participants, text, usedPrefix, command }) => {
-    if (!text) throw `Tag Member nya\nContoh: *#promote @frieren*`
+    if (!text) throw `Tag Member nya\nContoh: *${usedPrefix + command}* @frieren`
     let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
     if (/promote/i.test(command)) {
 	    await conn.groupParticipantsUpdate(m.chat, [users], 'promote')
-	    await m.reply(`Success Promote`)
+	    await m.reply(`Success ${command}`)
     } else if (/demote/i.test(command)) {
 	    await conn.groupParticipantsUpdate(m.chat, [users], 'demote')
-	    await m.reply(`Success Demote`)
+	    await m.reply(`Success ${command}`)
+    } else if (/add/i.test(command)) {
+	    await conn.groupParticipantsUpdate(m.chat, [users], 'add')
+	    await m.reply(`Success ${command}`)
+    } else if (/kick/i.test(command)) {
+	    await conn.groupParticipantsUpdate(m.chat, [users], 'remove')
+	    await m.reply(`Success ${command}`)
     }
 }
 handler.help = ['promote @user', 'demote @user']
