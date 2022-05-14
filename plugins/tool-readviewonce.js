877ec2dd72341143221800c201bf4c3a895f3c52
aniq <82928780+aniq12@users.noreply.github.com>
2022-05-14 03:51:51 +0000

    fix readviewonce (#326)

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,9 +1,20 @@
+let { downloadContentFromMessage } = (await import('@adiwajshing/baileys'));
+
 let handler = async (m, { conn }) => {
     if (!m.quoted) throw 'where\'s message?'
     if (m.quoted.mtype !== 'viewOnceMessage') throw 'Itu bukan pesan viewOnce'
-    const msg = await conn.loadMessage(m.quoted.id)
-    if (!msg) throw 'can\'t open message!'
-    await conn.copyNForward(m.chat, msg, true, { readViewOnce: true })
+    let msg = m.quoted.message
+    let type = Object.keys(msg)[0]
+    let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+    let buffer = Buffer.from([])
+    for await (const chunk of media) {
+        buffer = Buffer.concat([buffer, chunk])
+    }
+    if (/video/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
+    } else if (/image/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
+    }
 }
 
 handler.help = ['readviewonce']
