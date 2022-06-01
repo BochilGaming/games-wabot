934b2f549efdbe99ad663f08fb2351b2bca10254
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2022-06-01 11:01:31 +0700

    banned user

---
+++ plugins/owner-banuser.js
@@ -0,0 +1,18 @@
+import db from '../lib/database.js'
+
+let handler = async (m, { conn, text }) => {
+    if (!text) throw 'Siapa yang mau di banned?'
+    let who
+    if (m.isGroup) who = m.mentionedJid[0]
+    else who = m.chat
+    if (!who) throw 'Tag salah satu lah'
+    let users = db.data.users
+    users[who].banned = true
+    conn.reply(m.chat, `berhasil banned`, m)
+}
+handler.help = ['ban']
+handler.tags = ['owner']
+handler.command = /^ban$/i
+handler.rowner = true
+
+export default handler
