2142e306dc77f8e194a918b3e4b24f2ec3c3074c
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2022-06-01 16:15:51 +0700

    Banned user & unbanned user (#347)
    * banned user
    
    * unbanned user

---
+++ plugins/owner-unbanuser.js
@@ -0,0 +1,18 @@
+import db from '../lib/database.js'
+
+let handler = async (m, { conn, text}) => {
+    if (!text) throw 'Siapa yang mau di unbanned?'
+    let who
+    if (m.isGroup) who = m.mentionedJid[0]
+    else who = m.chat
+    if (!who) throw 'Tag salah satu lah'
+    let users = db.data.users
+    users[who].banned = false
+    conn.reply(m.chat, `berhasil unbanned`, m)
+}
+handler.help = ['unban @user']
+handler.tags = ['owner']
+handler.command = /^unban$/i
+handler.rowner = true
+
+export default handler
