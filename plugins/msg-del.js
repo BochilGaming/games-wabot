7d248f549f4eef7ed0da7431c42af2c21209b09e
aniq <82928780+aniq12@users.noreply.github.com>
2022-06-03 06:54:17 +0700

    Create msg-del.js

---
+++ plugins/msg-del.js
@@ -0,0 +1,14 @@
+import db from '../lib/database.js'
+
+let handler = async (m, { command, usedPrefix, text }) => {
+    if (!text) throw `Gunakan *${usedPrefix}listmsg* untuk melihat daftar nya`
+    let msgs = global.db.data.msgs
+    if (!text in msgs) throw `'${text}' tidak terdaftar di daftar pesan`
+    delete msgs[text]
+    m.reply(`Berhasil menghapus pesan di daftar pesan dengan nama '${text}'`)
+}
+handler.help = ['msg'].map(v => 'del' + v + ' <teks>')
+handler.tags = ['database']
+handler.command = /^delmsg$/
+
+export default handler
