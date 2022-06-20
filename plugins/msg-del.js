6a02aa4986690f6b97413c851043ae77ea492f96
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-06-20 12:07:03 +0700

    Update msg-del.js

--- plugins/msg-del.js
+++ plugins/msg-del.js
@@ -3,7 +3,7 @@ import db from '../lib/database.js'
 let handler = async (m, { command, usedPrefix, text }) => {
     if (!text) throw `Gunakan *${usedPrefix}listmsg* untuk melihat daftar nya`
     let msgs = global.db.data.msgs
-    if (!text in msgs) throw `'${text}' tidak terdaftar di daftar pesan`
+    if (!(text in msgs)) throw `'${text}' tidak terdaftar di daftar pesan`
     delete msgs[text]
     m.reply(`Berhasil menghapus pesan di daftar pesan dengan nama '${text}'`)
 }
