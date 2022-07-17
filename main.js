7b2fba95210ad024f9f70a7dc0b5592875bb7679
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-07-17 11:18:31 +0700

    Some bug fixes

--- main.js
+++ main.js
@@ -3,19 +3,11 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 process.on('uncaughtException', console.error)
 
-import './config.js';
-import { join } from 'path'
-import {
-  readdirSync,
-  statSync,
-  unlinkSync,
-} from 'fs'
+import './config.js'
+
 import {
   spawn
 } from 'child_process'
-import {
-  tmpdir
-} from 'os'
 import {
   protoType,
   serialize
@@ -23,11 +15,14 @@ import {
 import {
   plugins,
   filesInit,
-  reload
+  reload,
+  pluginFolder,
+  pluginFilter
 } from './lib/plugins.js'
 import Connection from './lib/connection.js'
 import Helper from './lib/helper.js'
 import db, { loadDatabase } from './lib/database.js'
+import clearTmp from './lib/clearTmp.js';
 
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
@@ -41,15 +36,11 @@ global.timestamp = {
   start: new Date
 }
 
-const __dirname = global.__dirname(import.meta.url)
-
 // global.opts['db'] = process.env['db']
 
 const conn = await Connection.conn
 
 // load plugins
-const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
-const pluginFilter = filename => /\.(mc)?js$/.test(filename)
 filesInit(pluginFolder, pluginFilter, conn).then(_ => console.log(Object.keys(plugins))).catch(console.error)
 
 Object.freeze(reload)
@@ -57,26 +48,14 @@ Object.freeze(reload)
 
 if (!opts['test']) {
   setInterval(async () => {
-    if (db.data) await db.write().catch(console.error)
-    if (opts['autocleartmp']) try {
-      clearTmp()
-    } catch (e) { console.error(e) }
+    await Promise.allSettled([
+      db.data ? db.write() : Promise.reject('db.data is null'),
+      (opts['autocleartmp'] || opts['cleartmp']) ? clearTmp() : Promise.resolve()
+    ])
     Connection.store.writeToFile(Connection.storeFile)
   }, 60 * 1000)
 }
 if (opts['server']) (await import('./server.js')).default(conn, PORT)
-
-
-function clearTmp() {
-  const tmp = [tmpdir(), join(__dirname, './tmp')]
-  const filename = []
-  tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-  return filename.map(file => {
-    const stats = statSync(file)
-    if (stats.isFile() && (Date.now() - stats.mtimeMs >= 1000 * 60 * 3)) return unlinkSync(file) // 3 minutes
-    return false
-  })
-}
 
 
 // Quick Test
