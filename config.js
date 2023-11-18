62fc7f46a7cc6d601f51b17c8fb9210973507dcd
Invincibility44 <151100337+Invincibility44@users.noreply.github.com>
2023-11-18 11:24:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
+  '62857139649630': 2348032309782{ // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
+  '6281390658325': 2348032309782{ // put your number here
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
-    name: 'Dave',
+  '6282256115584': 2348032309782{ // put your number here
+    name: 'demon',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
@@ -26,7 +26,7 @@ global.Owner = {
 }
 global.mods = ['13479805233'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
-global.APIs = { // API Prefix
+global.APIs = .{ // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
